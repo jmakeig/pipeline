5744ad772ce6860ad055a77281781d61344d8e52
Justin Makeig <jmpublic@makeig.com>
2023-09-18 21:06:39 +0000

    Fixes whitespace and adds comments

--- src/db/spanner/schema.sql
+++ src/db/spanner/schema.sql
@@ -1,27 +1,31 @@
+-- Drop, order matters
 DROP TABLE IF EXISTS events;
 DROP TABLE IF EXISTS workloads;
 DROP INDEX IF EXISTS customer_label;
 DROP TABLE IF EXISTS customers;
 
+-- Create, order matters
+-- customers
+
 CREATE TABLE IF NOT EXISTS customers (
-	--customer_id 		varchar(36) UNIQUE NOT NULL DEFAULT gen_random_uuid()::varchar(36),
-		customer_id 		varchar(36) NOT NULL DEFAULT spanner.generate_uuid()::varchar(36),
-  	label 					text NOT NULL,
-  	name_canonical	text,
-  	vector_id				text,
-    segment					text,
-  	industry				text,
+	--customer_id		varchar(36) UNIQUE NOT NULL DEFAULT gen_random_uuid()::varchar(36),
+	customer_id			varchar(36) NOT NULL DEFAULT spanner.generate_uuid()::varchar(36),
+	label						text NOT NULL,
+	name_canonical	text,
+	vector_id				text,
+  segment					text,
+	industry				text,
 	PRIMARY KEY (customer_id)
 );
 -- UNIQUE constraint
-
 CREATE UNIQUE INDEX customer_label ON customers (label);
 
+-- workloads
 
 CREATE TABLE IF NOT EXISTS workloads (
-	--workload_id 		varchar(36) UNIQUE NOT NULL DEFAULT gen_random_uuid()::varchar(36),
-	workload_id 		varchar(36) NOT NULL DEFAULT spanner.generate_uuid()::varchar(36),
- 	customer_id			varchar(36) NOT NULL REFERENCES customers(customer_id),
+	--workload_id		varchar(36) UNIQUE NOT NULL DEFAULT gen_random_uuid()::varchar(36),
+	workload_id			varchar(36) NOT NULL DEFAULT spanner.generate_uuid()::varchar(36),
+	customer_id			varchar(36) NOT NULL REFERENCES customers(customer_id),
 	product					text NOT NULL DEFAULT 'Cloud Spanner',
 	title						text,
 	description			text,
@@ -32,6 +36,7 @@ CREATE TABLE IF NOT EXISTS workloads (
 	PRIMARY KEY (workload_id)
 );
 
+-- events
 
 CREATE TABLE IF NOT EXISTS events (
 	--event_id			varchar(36) UNIQUE NOT NULL DEFAULT gen_random_uuid()::varchar(36),
