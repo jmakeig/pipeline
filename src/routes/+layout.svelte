183c18b74589cd8ae796267604a00bd72fb794bb
Justin Makeig <jmpublic@makeig.com>
2023-09-19 04:04:54 +0000

    Some clean-up

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	export const ssr = true;
+	export const csr = true;
+
+	if (!csr) {
+		console.warn(`Client-side rendering is DISABLED in root layout`);
+	}
+</script>
+
+<h1>Pipeline</h1>
+<slot />
