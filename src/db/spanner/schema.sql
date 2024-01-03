2c46782f73dc04fdcd671220cbf1ccf9c9a08d4f
Justin Makeig <jmpublic@makeig.com>
2024-01-03 23:17:25 +0000

    Tries to make foreign keys more rigorous Neither PostgreSQL nor Spanner support partially `NULL` composite foreign keys: https://stackoverflow.com/questions/52319723/oracle-partially-nullable-composite-foreign-key

--- src/db/spanner/schema.sql
+++ src/db/spanner/schema.sql
@@ -1,5 +1,6 @@
 -- Drop, order matters
 DROP TABLE IF EXISTS events;
+DROP INDEX IF EXISTS workload_customer;
 DROP TABLE IF EXISTS workloads;
 DROP INDEX IF EXISTS customer_label;
 DROP TABLE IF EXISTS customers;
@@ -13,18 +14,19 @@ CREATE TABLE IF NOT EXISTS customers (
 	label           text NOT NULL,
 	name_canonical  text,
 	vector_id       text,
-  segment         text,
+  	segment         text,
 	industry        text,
 	PRIMARY KEY (customer_id)
 );
 -- UNIQUE constraint
 CREATE UNIQUE INDEX customer_label ON customers (label);
 
+
 -- workloads
 CREATE TABLE IF NOT EXISTS workloads (
 	--workload_id   varchar(36) UNIQUE NOT NULL DEFAULT gen_random_uuid()::varchar(36),
 	workload_id     varchar(36) NOT NULL DEFAULT spanner.generate_uuid()::varchar(36),
-	customer_id     varchar(36) NOT NULL REFERENCES customers(customer_id),
+	customer_id     varchar(36) NOT NULL,
 	product         text NOT NULL DEFAULT 'Cloud Spanner',
 	title           text,
 	description     text,
@@ -32,16 +34,25 @@ CREATE TABLE IF NOT EXISTS workloads (
 	size            numeric,
 	win_date        date,
 	confidence      int,
-	PRIMARY KEY (workload_id)
+	--PRIMARY KEY (customer_id, workload_id),
+  	PRIMARY KEY (customer_id, workload_id), 
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
+-- UNIQUE constraint
+CREATE UNIQUE INDEX workload_customer ON workloads (customer_id, workload_id);
+
 
 -- events
 CREATE TABLE IF NOT EXISTS events (
 	--event_id      varchar(36) UNIQUE NOT NULL DEFAULT gen_random_uuid()::varchar(36),
 	event_id        varchar(36) NOT NULL DEFAULT spanner.generate_uuid()::varchar(36),
-	customer_id     varchar(36) NOT NULL REFERENCES customers(customer_id),
-	workload_id     varchar(36) REFERENCES workloads(workload_id),
+	customer_id     varchar(36) NOT NULL,
+	workload_id     varchar(36), -- FK below
 	timestamp       timestamptz NOT NULL DEFAULT now(),
-	PRIMARY KEY (event_id)
+  PRIMARY KEY (event_id),
+	--PRIMARY KEY (customer_id, workload_id, event_id),
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+  	-- https://stackoverflow.com/questions/52319723/oracle-partially-nullable-composite-foreign-key
+	FOREIGN KEY (workload_id) REFERENCES workloads(workload_id)
 );
 
