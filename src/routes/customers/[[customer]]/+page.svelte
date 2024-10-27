f244b338588d9f104f6e63b1c81f4eaf6d73399f
Justin Makeig <jmpublic@makeig.com>
2024-10-27 20:44:51 +0000

    Links customers with workloads and events

--- src/routes/customers/[[customer]]/+page.svelte
+++ src/routes/customers/[[customer]]/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
+	/** @type {import('./$types').PageData} */
+	export let data;
 </script>
 
 <h1>Customers</h1>
@@ -8,12 +8,16 @@
 	<thead>
 		<tr>
 			<th>Customer</th>
+			<th>Workloads</th>
+			<th>Events</th>
 		</tr>
 	</thead>
 	<tbody>
 		{#each data.customers as customer}
 			<tr>
 				<td><a href="/customers/{customer.label}">{customer.name}</a></td>
+				<td><a href="/workloads/{customer.label}">Workloads</a></td>
+				<td><a href="/events/{customer.label}">Events</a></td>
 			</tr>
 		{/each}
 	</tbody>
