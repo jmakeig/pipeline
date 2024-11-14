f388845f457de7847dc6c04db235db711a6b77f4
Justin Makeig <jmpublic@makeig.com>
2024-11-14 05:36:25 +0000

    Fixes workload type

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,7 @@ import * as api from '$lib/server/api';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
-	/** @type {Array<{workload: import('$lib/types').Workload, urgency: number}>} */
+	/** @type {Array<{workload: import('$lib/types').WorkloadDeep, urgency: number}>} */
 	const follow_ups = await api.get_workload_urgency();
 	/** @type {Array<{stage: number, name: string, workloads: {count: number, size: number?}}>} */
 	const stages = await api.get_stages_summary();
