9953e941f1d98b037bc73d2a41f12173a689d214
Justin Makeig <jmpublic@makeig.com>
2024-11-26 06:40:35 +0000

    Better types?

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,10 +1,11 @@
 /** @type {import('@sveltejs/kit').HandleServerError} */
 export async function handleError({ error, event, status, message }) {
 	console.error(error);
-	// return { message: error.message };
-	return {
-		message: error.message,
-		line: error.line,
-		code: error.code
-	};
+	if (error instanceof Error) {
+		return {
+			message: error.message,
+			line: error.line,
+			code: error.code
+		};
+	}
 }
