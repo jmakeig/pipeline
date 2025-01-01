e589b753f64c9c6e3fa0e131221a94e3583e5a8d
Justin Makeig <jmpublic@makeig.com>
2025-01-01 07:03:20 +0000

    Cleans up some cruft

--- src/routes/customers/[customer]/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { get_customer } from '$lib/server/api';
-import { error } from '@sveltejs/kit';
-
-/** @type {import('./$types').PageServerLoad} */
-export async function load({ params }) {
-	const customer = await get_customer(params.customer);
-	if (!customer) error(404, `Customer '${params.customer}' does not exist`);
-	return { customer };
-}
