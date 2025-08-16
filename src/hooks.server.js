8b74f612a0ee3621a9b91becc483403c66bf8910
Justin Makeig <jmpublic@makeig.com>
2025-08-16 01:51:34 +0000

    Adds route protection

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,8 +1,12 @@
+import { sequence } from '@sveltejs/kit/hooks';
 import { auth } from '$lib/server/api';
 import { is_invalid } from '$lib/validation';
+import { redirect } from '@sveltejs/kit';
+
+export const handle = sequence(auth_handle, protect_handle);
 
 /** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }) {
+async function auth_handle({ event, resolve }) {
 	// get cookies from browser
 	const auth_token = event.cookies.get('session');
 
@@ -30,6 +34,24 @@ export async function handle({ event, resolve }) {
 	console.log('Resolving with event.locals.user', event.locals.user);
 	// load page as normal
 	return await resolve(event);
+}
+/** @type {import('@sveltejs/kit').Handle} */
+async function protect_handle({ event, resolve }) {
+	console.log('protect_handle', event.locals.user);
+	console.log('event.route.id', event.route.id?.split('/'));
+	const route = event.route;
+	if (null === route || null === route.id) return resolve(event); // When is this true?
+	if (route.id.split('/').includes('(protected)')) {
+		console.warn('PROTECTED', route.id);
+		// TODO: Check roles
+		if (event.locals.user) return resolve(event);
+		return new Response(null, {
+			status: 302,
+			headers: { location: '/login' }
+		});
+	}
+	// https://github.com/mustofa-id/sveltekit-auth-hook-example/blob/4609c3870393cec4353ecbf3f6aa77d4832bada2/src/hooks/index.ts
+	return resolve(event);
 }
 
 /** @type {import('@sveltejs/kit').HandleServerError} */
