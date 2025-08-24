cdde736298a435ae431d9c1ed8823834e09c1b62
Justin Makeig <jmpublic@makeig.com>
2025-08-24 20:18:01 +0000

    Cleans up types

--- src/routes/(public)/+page.server.js
+++ src/routes/(public)/+page.server.js
@@ -1,28 +1,24 @@
 import * as api from '$lib/server/api';
 import { fail } from '@sveltejs/kit';
 
-/** @type {import('../$types').PageServerLoad} */
-export async function load({ parent }) {
-	/** @type {Array<{workload: import('$lib/types').WorkloadDeep, urgency: number}>} */
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
 	const follow_ups = await api.get_workload_urgency();
-	/** @type {Array<import('$lib/types').SalesStage & { workloads: {count: number, size: number?}}>} */
 	const stages = await api.get_stages_summary();
-	/** @type {number?} */
 	const size = await api.get_pipeline_size();
-
 	const customer_workloads = await api.get_customer_workloads();
 
 	return { follow_ups, stages, size, customer_workloads };
 }
 
-/** @satisfies {import('../$types').Actions} */
+/** @satisfies {import('./$types').Actions} */
 export const actions = {
 	quick_add: async ({ request }) => {
 		console.log('quick_add');
 		const form = await request.formData();
 		return fail(400, {
-			/** @type {import("$lib/types").Validation} */
-			validations: { for: 'customer_workload', message: 'Noooooo!' },
+			/** @type {import("$lib/types").Validation<{customer_workload: any}>[]} */
+			validations: [{ for: 'customer_workload', message: 'Noooooo!' }],
 			data: null
 		});
 	}
